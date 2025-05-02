10ab15c4131102e6a250a9dee7c0ba5a80223955
yadasai007 <yadasai007@gmail.com>
2025-05-02 10:46:26 -0400

    palindrome is added

--- src/main/java/org/example/LinkedList.java
+++ src/main/java/org/example/LinkedList.java
@@ -95,21 +95,61 @@ public class LinkedList {
         System.out.println(temp.number);
     }
 
-    public LinkedList reverseLinkedList()
-    {
-        LinkedListNode previousNode=null;
-        LinkedListNode currentNode=this.first;
-        LinkedListNode nextNode;
-        while(currentNode!=null)
-        {
-            nextNode=currentNode.nextNode;
-            currentNode.nextNode=previousNode;
-            previousNode=currentNode;
-            currentNode=nextNode;
+    public LinkedList reverseLinkedList() {
+        if (this.first == null) {
+            return new LinkedList(null); // Handle empty list
         }
-        return new LinkedList(previousNode);
+
+        LinkedListNode originalCurrent = this.first;
+        LinkedListNode newHead = null;
+
+        while (originalCurrent != null) {
+            // Create a new node with the same data
+            LinkedListNode newNode = new LinkedListNode(originalCurrent.number);
+
+            // Insert at the head of the new list (to reverse)
+            newNode.nextNode = newHead;
+            newHead = newNode;
+
+            // Move to the next node in the original list
+            originalCurrent = originalCurrent.nextNode;
+        }
+
+        return new LinkedList(newHead);
     }
 
+    public String isPalindrome() {
+        if(this.first==null)
+            return "is Palindrome";
+         LinkedListNode fast=this.first;
+         LinkedListNode slow=this.first;
+         if(fast.nextNode!=null)
+         {
+             fast=fast.nextNode;
+         }
+         else
+         {
+             return "is Palindrome";
+         }
+         while(fast!=null && fast.nextNode!=null)
+         {
+             fast=fast.nextNode.nextNode;
+             slow=slow.nextNode;
+         }
+         fast=this.first;
+         LinkedList reverse=new LinkedList(slow).reverseLinkedList();
+         LinkedListNode reverseNode=reverse.first;
+         while(reverseNode!=null)
+         {
+             if(!reverseNode.number.equals(fast.number))
+             {
+                 return "is not Palindrome";
+             }
+             reverseNode=reverseNode.nextNode;
+             fast=fast.nextNode;
+         }
+        return "is Palindrome";
+    }
 
     public static void main(String[] args) {
         LinkedList linkedList=new LinkedList();
@@ -118,23 +158,45 @@ public class LinkedList {
         {
             linkedList.insert_at_end(number);
         }
+        System.out.println("Insertion at the end");
         linkedList.display();
         Integer[] beginNumbers={3,2,1,0,-1};
         for (Integer number:beginNumbers)
         {
             linkedList.insert_at_begin(number);
         }
+        System.out.println("Insertion at the begin");
         linkedList.display();
         linkedList.delete_at_begin();
+        System.out.println("Deletion at the begin");
         linkedList.display();
         linkedList.delete_at_end();
+        System.out.println("Deletion at the end");
         linkedList.display();
-        linkedList.insert_at_pos(4,11);
+        Integer position=4;
+        Integer position_number=11;
+        linkedList.insert_at_pos(position,position_number);
+        System.out.println("Insertion at the position "+position);
         linkedList.display();
-        linkedList.delete_at_pos(4);
+        linkedList.delete_at_pos(position);
+        System.out.println("Deletion at the position "+position);
         linkedList.display();
         LinkedList reverse=linkedList.reverseLinkedList();
+        System.out.println("Linked List before reverse");
+        linkedList.display();
+        System.out.println("Linked List after reverse");
         reverse.display();
+        LinkedList linkedList1=new LinkedList();
+        Integer[] palindromeNumbers={3,2,1,2,3};
+        for (Integer number:palindromeNumbers)
+        {
+            linkedList1.insert_at_begin(number);
+        }
+        System.out.println();
+        linkedList1.display();
+        System.out.println("Linked List "+linkedList1.isPalindrome());
+        linkedList.display();
+        System.out.println("Linked List "+linkedList.isPalindrome());
     }
 
 }
